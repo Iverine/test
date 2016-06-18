ec60ac68349e68fb43ccfa231f5ba48f630b6335
Andre <andre@nitschke-marl.de>
2016-06-18 23:43:55 +0200

    Programm zum Anlegen der DAten

---
+++ adresse_anlegen.php
@@ -0,0 +1,38 @@
+<?php
+
+// Verbindung zur Datenbank aufbauen
+include "db.inc.php";
+
+// Daten aus der Eingabemaske holen
+$vorname = $_POST["vorname"];
+$nachname = $_POST["nachname"];
+$strasse = $_POST["strasse"];
+$ort = $_POST["ort"];
+$telefon = $_POST["telefon"];
+$email = $_POST["email"];
+
+
+
+class adresse {
+ 
+ private $vorname, $name, $strasse, $ort, $telefon, $email;
+
+ public function adresse($name,$vorname, $handy) {
+      $this->vorname = $vorname;
+      $this->name = $name;
+      $this->strasse = $strasse;
+      $this->ort = $ort;
+      $this->telefon = $telefon;
+      $this->email = $email;
+ }
+ private function db_setAdress(){
+  $anix_query = "INSERT INTO adressliste (vorname, nachname, strasse, ort, telefon, email) VALUES ('$vorname', '$nachname', '$strasse', '$ort', '$telefon', '$email')";
+  $anix_result = mysqli_query($db, $aniox_query);
+ }
+}
+
+
+$neuerKunde1 = new adresse($vorname, $nachname, $strasse, $ort, $telefon, $email);
+
+
+?>
