9b3023c5182dc35fcb5badb73a0dfe4eff78815c
Andre <andre@nitschke-marl.de>
2016-06-19 01:53:58 +0200

    Fehler korrigiert

--- adresse_anlegen.php
+++ adresse_anlegen.php
@@ -15,20 +15,24 @@ $email = $_POST["email"];
 
 class adresse {
  
- private $vorname, $name, $strasse, $ort, $telefon, $email;
+ private $vorname, $nachname, $strasse, $ort, $telefon, $email;
 
- public function adresse($name,$vorname, $handy) {
+ public function adresse($vorname, $nachname, $strasse, $ort, $telefon, $email) {
       $this->vorname = $vorname;
-      $this->name = $name;
+      $this->nachname = $nachname;
       $this->strasse = $strasse;
       $this->ort = $ort;
       $this->telefon = $telefon;
       $this->email = $email;
-	echo "test";
+
+      $ergebnis = db_setAdress($vorname, $nachname, $strasse, $ort, $telefon, $email);
+      echo $ergebnis;
  }
- private function db_setAdress(){
+ private function db_setAdress($vorname, $nachname, $strasse, $ort, $telefon, $email){
+ echo "Funktion setAdress";
   $anix_query = "INSERT INTO adressliste (vorname, nachname, strasse, ort, telefon, email) VALUES ('$vorname', '$nachname', '$strasse', '$ort', '$telefon', '$email')";
-  $anix_result = mysqli_query($db, $aniox_query);
+  $anix_result = mysqli_query($db, $anix_query);
+ return;
  }
 }
 
