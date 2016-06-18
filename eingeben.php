ab100af7754a2859fac4db0b8f50dd600160848e
Andre <andre@nitschke-marl.de>
2016-06-18 22:37:09 +0200

    Eingabemaske

---
+++ eingeben.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+ <title>Adresse eingeben</title>
+ </head>
+ 
+ <body>
+  <form action=adresse_anlegen.php" method="post">
+      Vorname: <input name="vorname" type="text" size="30" ><br><br>
+      Nachname: <input name="nachname" type="text" size="30"><br><br>
+      Strasse: <input name="strasse" type="text" size="30"><br><br>
+      Ort: <input name="ort" type="text" size="30"><br><br>
+      Telefon: <input name="telefon" type="test" size="30"><br><br>
+      eMail: <input name="email" type="email" size="30"><br><br>
+      <input type="submit" value=" Projekt Erstellen ">
+  </form>
+ </body>
+</html>
