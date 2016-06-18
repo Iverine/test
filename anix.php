4756b252339fbdcd6121a95df10addffa8c70898
Andre <andre@nitschke-marl.de>
2016-06-18 22:20:32 +0200

    Startseite

---
+++ anix.php
@@ -0,0 +1,13 @@
+<htm>
+ <head>
+ <title>WebANIX</title>
+ </head>
+ 
+ <body>
+  <h1>WebANIX</h1> <br>
+  <hr>
+  <a href="eingeben.php">Adresse anlegen</a><br><br>
+  <a href="suchen.php">Adresse suchen</a><br><br>
+  <a href="liste.php">Adressliste</a>
+ </body>
+</html>
